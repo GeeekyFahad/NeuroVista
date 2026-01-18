acbb4de206a4f7bb9819aa8b01ab5b751c2c8114
Fahad <155130250+GeeekyFahad@users.noreply.github.com>
2026-01-18 13:16:33 +0530

    Update dataset.py

--- model/dataset.py
+++ model/dataset.py
@@ -1,33 +1,59 @@
-from torch.utils.data import Dataset
 import os
+from PIL import Image
 import torch
-from preprocess import preprocess
+from torch.utils.data import Dataset
+from torchvision import transforms
 
-class OASISDataset(Dataset):
-    def __init__(self, root_dir):
-        self.root = root_dir
-        self.paths = []
-        self.labels = []
-        classes = {
-            "Non Demented":0,
-            "Very mild Dementia":1,
-            "Mild Dementia":2,
-            "Moderate Dementia":3
+class AlzheimerDataset(Dataset):
+    def __init__(self, root_dir, augment=False):
+        self.root_dir = root_dir
+        self.augment = augment
+
+        self.classes = {
+            "Non Demented": 0,
+            "Very mild Dementia": 1,
+            "Mild Dementia": 2,
+            "Moderate Dementia": 3
         }
 
-        for cls in classes:
+        self.image_paths = []
+        self.labels = []
+
+        for cls in self.classes:
             folder = os.path.join(root_dir, cls)
             for file in os.listdir(folder):
-                if file.endswith(".nii") or file.endswith(".nii.gz"):
-                    self.paths.append(os.path.join(folder, file))
-                    self.labels.append(classes[cls])
+                if file.lower().endswith((".jpg", ".jpeg", ".png")):
+                    self.image_paths.append(os.path.join(folder, file))
+                    self.labels.append(self.classes[cls])
+
+        self.base_transform = transforms.Compose([
+            transforms.Resize((224, 224)),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.5], std=[0.5])
+        ])
+
+        self.aug_transform = transforms.Compose([
+            transforms.Resize((224, 224)),
+            transforms.RandomHorizontalFlip(),
+            transforms.RandomRotation(10),
+            transforms.ColorJitter(brightness=0.2, contrast=0.2),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.5], std=[0.5])
+        ])
 
     def __len__(self):
-        return len(self.paths)
+        return len(self.image_paths)
 
     def __getitem__(self, idx):
-        path = self.paths[idx]
-        x = preprocess(path)
-        x = x.unsqueeze(0)
-        y = torch.tensor(self.labels[idx]).long()
-        return x, y
+        img_path = self.image_paths[idx]
+        label = self.labels[idx]
+
+        image = Image.open(img_path).convert("RGB")  # force 3-channel
+
+        if self.augment:
+            image = self.aug_transform(image)
+        else:
+            image = self.base_transform(image)
+
+        label = torch.tensor(label).long()
+        return image, label
